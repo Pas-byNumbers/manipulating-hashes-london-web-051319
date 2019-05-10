454e2d922bb3b9c3456956bf19f4479470dfb2b9
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-10 18:17:35 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,10 +15,11 @@ def first_challenge
 
   #your code here
 contacts.collect do |person, data|
-  data.collect do |key, value|
-    value.delete("strawberry")
+  if attribute==:favorite_icecream_flavors
+        value.delete_if {|flavor| flavor=="strawberry"}
+       
+      end
   end
-end
 
   #remember to return your newly altered contacts hash!
   contacts
